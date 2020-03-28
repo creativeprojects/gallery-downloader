7e2bd839bb2ca4c41752a478bedef682282f8104
Fred <fred@creativeprojects.tech>
2020-03-28 19:39:48 +0000

    Serve gzip files for testing

--- server/main.go
+++ server/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"compress/gzip"
 	"flag"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path"
+	"strings"
 )
+
+type gzipResponseWriter struct {
+	io.Writer
+	http.ResponseWriter
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
 
 func main() {
 	var root, port string
@@ -23,8 +35,16 @@ func main() {
 	log.Printf("Serving files from '%s' (use -root option to change the default)", root)
 	fs := http.FileServer(http.Dir(root))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.RequestURI)
-		fs.ServeHTTP(w, r)
+		log.Printf("%s - %d - %s - %s", r.Method, len(r.Header), r.RequestURI, r.Referer())
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			fs.ServeHTTP(w, r)
+			return
+		}
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+		gzw := gzipResponseWriter{Writer: gz, ResponseWriter: w}
+		fs.ServeHTTP(gzw, r)
 	})
 
 	log.Printf("Listening on :%s (use -port to change the default)...", port)
