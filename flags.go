0fb54507f8d502acf0c685dd2daf27e4cf786133
Fred <fred@creativeprojects.tech>
2020-03-28 23:06:44 +0000

    Update flags description

--- flags.go
+++ flags.go
@@ -27,14 +27,14 @@ var (
 func loadFlags() Flags {
 	flags := Flags{}
 	flag.StringVar(&flags.ConfigFile, "config", "config.json", "configuration file")
-	flag.StringVar(&flags.Source, "source", "", "source html")
+	flag.StringVar(&flags.Source, "source", "", "source HTML gallery")
 	flag.StringVar(&flags.Base, "base", "", "base URL when downloading relative images")
 	flag.StringVar(&flags.Type, "type", galleryTypes[0], "type of gallery ("+strings.Join(galleryTypes, ", ")+")")
 	flag.StringVar(&flags.Output, "output", "", "output folder to store pictures")
-	flag.StringVar(&flags.Referer, "referer", "", "referer header when downloading images from a local html file")
-	flag.StringVar(&flags.User, "user", "", "user (for html loader only)")
-	flag.StringVar(&flags.Password, "password", "", "password (for html loader only)")
-	flag.IntVar(&flags.Wait, "wait", 3000, "wait a maximum of n milliseconds before downloading the next image")
+	flag.StringVar(&flags.Referer, "referer", "", "referer header for HTML file, or for downloading images from a local HTML file")
+	flag.StringVar(&flags.User, "user", "", "user (if the http server needs basic authentication)")
+	flag.StringVar(&flags.Password, "password", "", "password (if the http server needs basic authentication)")
+	flag.IntVar(&flags.Wait, "wait", 3000, "wait between 1000 and n milliseconds before downloading the next image. Use 0 to deactivate")
 	flag.Parse()
 	return flags
 }
