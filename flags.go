045b20b63215221a429d31dd7f170f25f21304e7
Fred <fred@creativeprojects.tech>
2020-10-04 17:30:55 +0100

    change flag name

--- flags.go
+++ flags.go
@@ -35,8 +35,8 @@ func loadFlags() Flags {
 	flag.StringVar(&flags.Referer, "referer", "", "referer header for HTML file, or for downloading images from a local HTML file")
 	flag.StringVar(&flags.User, "user", "", "user (if the http server needs basic authentication)")
 	flag.StringVar(&flags.Password, "password", "", "password (if the http server needs basic authentication)")
-	flag.IntVar(&flags.WaitMin, "wait-min", 300, "wait n milliseconds minimum before downloading the next image. Use 0 to deactivate")
-	flag.IntVar(&flags.WaitMax, "wait-max", 1000, "wait n milliseconds maximum before downloading the next image. Use 0 to deactivate")
+	flag.IntVar(&flags.WaitMin, "min-wait", 1000, "wait n milliseconds minimum before downloading the next image. Use 0 to deactivate")
+	flag.IntVar(&flags.WaitMax, "max-wait", 3000, "wait n milliseconds maximum before downloading the next image. Use 0 to deactivate")
 	flag.Parse()
 	return flags
 }
